79ac9647b33a4b1a13a9e7ae3730cb8f83c71552
Shahidullah-Kaisar <shkaisar2002@gmail.com>
2025-05-27 13:58:18 +0600

    update admin ui

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Route, Routes } from "react-router-dom";
@@ -6,27 +6,51 @@ import Add from "./pages/Add";
 import List from "./pages/List";
 import Order from "./pages/Orders";
 import Login from "./components/Login";
+import { ToastContainer} from 'react-toastify';
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+console.log('backendUrl:',backendUrl);
+
 
 const App = () => {
 
-  const [token, setToken] = useState("");
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : '');
+  console.log('token comming from login.jsx:', token);
+
+  useEffect(()=>{
+    localStorage.setItem('token', token);
+  },[token])
 
   return (
     <div className="bg-gray-50 min-h-screen">
+
+      <ToastContainer
+        position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={false}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+      
       {token === "" ? (
-        <Login></Login>
+        <Login setToken = {setToken}/>
       ) : (
         <>
-          <Navbar></Navbar>
+          <Navbar setToken = {setToken} />
           <hr />
           <div className="flex w-full">
             <Sidebar></Sidebar>
 
             <div className="w-[70%] mx-auto ml-[max(5vw,25px)] my-8 text-gray-600 text-base">
               <Routes>
-                <Route path="/add" element={<Add></Add>}></Route>
-                <Route path="/list" element={<List></List>}></Route>
-                <Route path="/orders" element={<Order></Order>}></Route>
+                <Route path="/add" element={<Add token = {token}/>}></Route>
+                <Route path="/list" element={<List token = {token}/>}></Route>
+                <Route path="/orders" element={<Order token = {token}/>}></Route>
               </Routes>
             </div>
           </div>
--- admin/src/pages/Add.jsx
+++ admin/src/pages/Add.jsx
@@ -1,10 +1,70 @@
 import React from 'react';
+import { assets } from '../assets/assets';
 
 const Add = () => {
     return (
-        <div>
-            Add
-        </div>
+        <form className='flex flex-col w-full items-start gap-3'>
+
+            <div>
+                <p className='mb-2'>Upload Image</p>
+
+                <div className='flex gap-2'>
+                    <label htmlFor="image1">
+                        <img className='w-20' src={assets.upload_area} alt="" />
+                        <input type="file" name="" id="image1" hidden />
+                    </label>
+                    <label htmlFor="image2">
+                        <img className='w-20' src={assets.upload_area} alt="" />
+                        <input type="file" name="" id="image2" hidden />
+                    </label>
+                    <label htmlFor="image4">
+                        <img className='w-20' src={assets.upload_area} alt="" />
+                        <input type="file" name="" id="image4" hidden />
+                    </label>
+                    <label htmlFor="image4">
+                        <img className='w-20' src={assets.upload_area} alt="" />
+                        <input type="file" name="" id="image4" hidden />
+                    </label>
+                </div>
+            </div>
+
+            <div className='w-full'>
+                <p className='mb-2'>Product name</p>
+                <input className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Type here'/>
+            </div>
+            <div className='w-full'>
+                <p className='mb-2'>Product Description</p>
+                <textarea className='w-full max-w-[500px] px-3 py-2' type="text" placeholder='Write content here'></textarea>
+            </div>
+
+            <div>
+
+                <div>
+                    <p>Product Category</p>
+                    <select>
+                        <option value="Men">Men</option>
+                        <option value="Women">Women</option>
+                        <option value="Kids">Kids</option>
+                    </select>
+                </div>
+
+                <div>
+                    <p>Sub Category</p>
+                    <select>
+                        <option value="Topwear">Topwear</option>
+                        <option value="Bottomwear">Bottomwear</option>
+                        <option value="Winterwear">Winterwear</option>
+                    </select>
+                </div>
+
+                <div>
+                    <p>Product Price</p>
+                    <input type="number" placeholder='24' />
+                </div>
+
+            </div>
+
+        </form>
     );
 };
 
