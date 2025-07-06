db3ab44f3669f426df9694e5125a6137f38a5785
Shahidullah-Kaisar <shkaisar2002@gmail.com>
2025-07-06 22:11:53 +0600

    payment method add

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -75,16 +75,29 @@ const PlaceOrder = () => {
 
     switch (method) {
         case 'cod':
-            const response = await axios.post( backendUrl + '/api/order/place', orderData, { headers: { token }})
-            
-            if(response.data.success){
-                setCartItems( {} )
-                navigate('/orders')
-                toast.success("Order Confirmed")
-            }else {
-                toast.error(response.data.message)
-            }
-            break;
+          const response = await axios.post( backendUrl + '/api/order/place', orderData, { headers: { token }})
+          
+          if(response.data.success){
+              setCartItems( {} )
+              navigate('/orders')
+              toast.success("Order Confirmed")
+          }else {
+              toast.error(response.data.message)
+          }
+          break;
+
+        case 'stripe':
+          const responseStripe = await axios.post(backendUrl + '/api/order/stripe', orderData, {headers: { token }})
+
+          if(responseStripe.data.success) {
+            const { session_url } = responseStripe.data
+            console.log("session url", session_url)
+            window.location.replace( session_url )
+
+          }else{
+            toast.error(responseStripe.data.message)
+          }
+          break;
         
         default:
             break;
